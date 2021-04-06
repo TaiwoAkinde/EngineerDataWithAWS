68269ee4688954b83fc5996eb6e0ad287d674976
Taiwo Akinde <oladeleakinde@gmail.com>
2021-04-06 17:24:41 +0100

    uploading scrapped data to s3 bucket

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # EngineerDataWithAWS
-A simple project to demonstrate basic data engineering process with python and AWS
+###A simple project to demonstrate basic data engineering process with Python and AWS
+
+# URL: https://realpython.com/pipenv-guide/#pipenv-introduction
+
+-- create a corresponding repo on GitHub
+
+-- pip install pipenv [Run this on your PC for package management like the "npm" in javascript]
+
+-- create your project directory on local pc
+   -> git init
+   -> Open your IDE from within the project folder
+   >> Shift + Right Click >> Open cmd or Open Powershell
+     -> In your IDE terminal:
+        # use pipenv to create a Python 3 (--three) virtualenv for our project
+        >> pipenv --three
+        # To activate this project's virtualenv, run pipenv shell.
+        >> pipenv shell
+        # install dependency(ies) on our project
+        >> pipenv install ******
+        # Let’s say you also have some unit tests for this awesome application, and you want to use pytest for running them. You don’t need pytest in production so you can specify that this dependency is only for development with the --dev argument
+        >> pipenv install pytest --dev
+
+
+-- link your local repo to GitHub
+  -> https://docs.github.com/en/github/importing-your-projects-to-github/adding-an-existing-project-to-github-using-the-command-line
+  
+
+----> Project Description:
+-- Simple Data Engineering using:
+    - Python
+    - Boto3 (AWS SDK for Python)
+
+    # Create a user and assign appropriate rights on AWS IAM for development purposed
+    # get the :
+      .. aws_access_key_id
+      .. aws_secret_access_key
+      store these securely and reference in your main app
+
+
+-----> Moving to production
+        # Okay, so let’s say you’ve got everything working in your local development environment and you’re ready to push it to production. To do that, you need to lock your environment so you can ensure you have the same one in production:
+        >> pipenv lock
+        # Now, once you get your code and Pipfile.lock in your production environment, you should install the last successful environment recorded:
+        >> pipenv install --ignore-pipfile
--- app.py
+++ app.py
@@ -2,43 +2,64 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import boto3
-import json
-#from auth import ACCESS_KEY, SECRET_KEY
+import json 
+from auth import ACCESS_KEY_ID, SECRET_ACCESS_KEY, REGION
 
 """
     GET DATA FROM NEWS SITE
 """
 def get_news_items():
-        
-    soup = BeautifulSoup(requests.get('https://www.newsnow.co.uk/h/World+News').text, 'lxml')
-    csv_file = open('data.csv', 'w', newline='', encoding="utf-8")
-    writer = csv.writer(csv_file)
-    writer.writerow(['News Link', 'News Headline'])
 
-    articles_2 = soup.find_all(lambda tag: tag.name == 'a' and tag.get('class') == ['hll'])
-    news_object = {}
-    news_list = []
+    try:
+                
+        soup = BeautifulSoup(requests.get('https://www.newsnow.co.uk/h/World+News').text, 'lxml')
+        csv_file = open('data.csv', 'w', newline='', encoding="utf-8")
+        writer = csv.writer(csv_file)
+        writer.writerow(['News Link', 'News Headline'])
 
-    for article in articles_2:
-        if article['href'][:6] == 'https:':
+        articles_2 = soup.find_all(lambda tag: tag.name == 'a' and tag.get('class') == ['hll'])
+        news_object = {}
+        news_list = []
 
-            """Jsonify Data for Dynamo DB"""
-            news_object['_link'] = article['href']
-            news_object['_headline'] = article.text
+        for article in articles_2:
+            if article['href'][:6] == 'https:':
 
-            news_list.append(news_object.copy())
+                """Jsonify Data for Dynamo DB"""
+                news_object['_link'] = article['href']
+                news_object['_headline'] = article.text
 
-            """Write Data to CSV for S3 Bucket"""
-            writer.writerow([article['href'], article.text])   
+                news_list.append(news_object.copy())
 
-    csv_file.close()
+                """Write Data to CSV for S3 Bucket"""
+                writer.writerow([article['href'], article.text])   
+
+        csv_file.close()
+    except Exception as e:
+        return e
+
     return json.dumps(news_list)
 
+"""PUT DATA IN S3 BUCKET"""
 def put_data_in_s3_bucket():
-    pass
+    client = boto3.client(
+        's3',
+        aws_access_key_id = ACCESS_KEY_ID,
+        aws_secret_access_key = SECRET_ACCESS_KEY,
+        region_name = REGION,
+    )
+    try:
+        client.create_bucket(Bucket = 'python-aws-data-engineering')
 
+        with open("data.csv", "rb") as file:
+            client.upload_fileobj(file, 'python-aws-data-engineering', "data.csv")
 
+    except Exception as e:
+        return e
+
+    return {'status_code': 200, 'Message': f'{file.name} Uploaded Successfully'}
+
+"""PUT DATA IN DYNAMO DB"""
 def put_data_in_dynamo_db():
     pass
 
-get_news_items()
+print(put_data_in_s3_bucket())
